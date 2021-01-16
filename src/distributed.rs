4f4d75440c46dd1fd8130ccba5dfe707c15b6357
sun <huachuang20@gmail.com>
2021-01-16 14:26:33 +0800

    rename local

--- src/distributed.rs
+++ src/distributed.rs
@@ -1,160 +1,125 @@
-use rusqlite::{types::Value, Connection, NO_PARAMS};
-use std::path::PathBuf;
-use tdn_types::primitive::{new_io_error, Result};
+use postcard::{from_bytes, to_allocvec};
+use serde::{de::DeserializeOwned, Serialize};
+use std::{
+    io::{Error, ErrorKind, Result},
+    path::PathBuf,
+};
+use tdn_types::primitive::DEFAULT_STORAGE_DIR;
+use tdn_types::storage::Storage;
 
-pub enum DsValue {
-    Null,
-    Integer(i64),
-    Real(f64),
-    Text(String),
-    Blob(Vec<u8>),
+fn new_error(s: &str) -> Error {
+    Error::new(ErrorKind::Other, s)
 }
 
-impl DsValue {
-    pub fn is_none(&self) -> bool {
-        match self {
-            DsValue::Null => true,
-            _ => false,
-        }
+pub fn open_db(name: &str) -> Result<LocalDB> {
+    let mut path = DEFAULT_STORAGE_DIR.clone();
+    path.push(name);
+    LocalDB::open_absolute(&path)
+}
+
+pub fn open_absolute_db(mut path: PathBuf, name: &str) -> Result<LocalDB> {
+    path.push(name);
+    LocalDB::open_absolute(&path)
+}
+
+pub struct LocalDB {
+    tree: sled::Db,
+}
+
+impl Storage for LocalDB {
+    type Key = Vec<u8>;
+
+    fn read<T: Serialize + DeserializeOwned>(&self, k: &Self::Key) -> Option<T> {
+        self.tree
+            .get(k)
+            .ok()
+            .map(|v| v)
+            .flatten()
+            .map(|v| from_bytes(&v).ok())
+            .flatten()
     }
 
-    pub fn as_str(&self) -> &str {
-        match self {
-            DsValue::Text(s) => &s,
-            _ => "",
-        }
+    fn write<T: Serialize + DeserializeOwned>(&self, k: &Self::Key, t: &T) -> Result<()> {
+        to_allocvec(t)
+            .map_err(|_e| new_error("db serialize error!"))
+            .and_then(|bytes| {
+                self.tree
+                    .insert(k, bytes)
+                    .map_err(|_e| new_error("db write failure!"))
+                    .and_then(|_| {
+                        self.tree.flush()?;
+                        Ok(())
+                    })
+            })
     }
 
-    pub fn as_bool(&self) -> bool {
-        match self {
-            DsValue::Integer(i) => i == &1i64,
-            _ => false,
-        }
+    fn update<T: Serialize + DeserializeOwned>(&self, k: &Vec<u8>, t: &T) -> Result<()> {
+        to_allocvec(&t)
+            .map_err(|_e| new_error("db serialize error!"))
+            .and_then(|bytes| {
+                let old = self.tree.get(&k).ok().map(|v| v).flatten();
+                if old.is_none() {
+                    self.tree
+                        .insert(k, bytes)
+                        .map_err(|_e| new_error("db write failure!"))
+                        .and_then(|_| {
+                            self.tree.flush()?;
+                            Ok(())
+                        })
+                } else {
+                    self.tree
+                        .compare_and_swap(k, Some(old.unwrap()), Some(bytes))
+                        .map_err(|_e| new_error("db swap failure!"))
+                        .and_then(|_| {
+                            self.tree.flush()?;
+                            Ok(())
+                        })
+                }
+            })
     }
 
-    pub fn as_i64(self) -> i64 {
-        match self {
-            DsValue::Integer(i) => i,
-            _ => 0,
-        }
-    }
-
-    pub fn as_string(self) -> String {
-        match self {
-            DsValue::Text(s) => s,
-            _ => "".to_owned(),
-        }
-    }
-
-    pub fn as_f64(self) -> f64 {
-        match self {
-            DsValue::Real(f) => f,
-            _ => 0.0,
-        }
-    }
-
-    pub fn as_vec(self) -> Vec<u8> {
-        match self {
-            DsValue::Blob(v) => v,
-            _ => vec![],
+    fn delete<T: Serialize + DeserializeOwned>(&self, k: &Self::Key) -> Result<T> {
+        let result = self.read::<T>(k);
+        if result.is_some() {
+            self.tree
+                .remove(k)
+                .map_err(|_e| new_error("db delete error"))
+                .and_then(|_| {
+                    self.tree.flush()?;
+                    Ok(())
+                })?;
+            Ok(result.unwrap())
+        } else {
+            Err(new_error("db delete key not found!"))
         }
     }
 }
 
-impl From<Value> for DsValue {
-    fn from(value: Value) -> DsValue {
-        match value {
-            Value::Null => DsValue::Null,
-            Value::Integer(i) => DsValue::Integer(i),
-            Value::Real(i) => DsValue::Real(i),
-            Value::Text(s) => DsValue::Text(s),
-            Value::Blob(v) => DsValue::Blob(v),
-        }
+impl LocalDB {
+    pub fn open_absolute(path: &PathBuf) -> Result<LocalDB> {
+        let tree = sled::open(path).map_err(|_e| new_error("db open failure!"))?;
+        Ok(LocalDB { tree })
+    }
+
+    fn _flush(&self) -> Result<()> {
+        //smol::spawn(self.tree.flush_async());
+        //Ok(())
+        todo!();
     }
 }
 
-pub struct DStorage {
-    connect: Connection,
-}
+#[test]
+fn test_db_file() {
+    let name = "test_db";
+    let key = vec![1, 2, 3, 4];
+    let value = "A".to_owned();
+    let value_b = "B".to_owned();
 
-impl DStorage {
-    #[inline]
-    pub fn open(path: PathBuf) -> Result<Self> {
-        let connect =
-            Connection::open(path).map_err(|_e| new_io_error("database open failure."))?;
-        Ok(DStorage { connect })
-    }
-
-    /// tmp use.
-    #[inline]
-    pub fn execute(&self, sql: &str) -> Result<usize> {
-        self.connect
-            .execute(sql, NO_PARAMS)
-            .map_err(|_e| new_io_error("database execute failure."))
-    }
-
-    /// tmp use.
-    #[inline]
-    pub fn query(&self, sql: &str) -> Result<Vec<Vec<DsValue>>> {
-        let mut stmt = self
-            .connect
-            .prepare(sql)
-            .map_err(|_e| new_io_error("database prepare failure."))?;
-        let mut rows = stmt
-            .query(NO_PARAMS)
-            .map_err(|_e| new_io_error("database query failure."))?;
-
-        let mut matrix: Vec<Vec<DsValue>> = Vec::new();
-        while let Some(row) = rows
-            .next()
-            .map_err(|_e| new_io_error("database query failure."))?
-        {
-            let mut values: Vec<DsValue> = Vec::new();
-            for i in 0..row.column_count() {
-                values.push(
-                    row.get::<usize, Value>(i)
-                        .map_err(|_e| new_io_error("database query row failure."))?
-                        .into(),
-                );
-            }
-            matrix.push(values);
-        }
-
-        Ok(matrix)
-    }
-
-    /// tmp use. return the insert id.
-    #[inline]
-    pub fn insert(&self, sql: &str) -> Result<i64> {
-        self.execute(sql)?;
-        Ok(self.connect.last_insert_rowid())
-    }
-
-    /// tmp use.
-    #[inline]
-    pub fn update(&self, sql: &str) -> Result<usize> {
-        self.execute(sql)
-    }
-
-    /// tmp use.
-    #[inline]
-    pub fn delete(&self, sql: &str) -> Result<usize> {
-        self.execute(sql)
-    }
-
-    /// tmp use.
-    #[inline]
-    pub fn flush(&self) -> Result<()> {
-        self.connect.flush_prepared_statement_cache();
-        Ok(())
-    }
-
-    /// tmp use.
-    #[inline]
-    pub fn close(self) -> Result<()> {
-        self.flush()?;
-        self.connect
-            .close()
-            .map_err(|_e| new_io_error("database close failure."))
-    }
+    let db = open_db(name).unwrap();
+    assert_eq!(db.write(&key, &value).ok(), Some(()));
+    assert_eq!(db.read::<String>(&key), Some(value.clone()));
+    assert_eq!(db.update::<String>(&key, &value_b).ok(), Some(()));
+    assert_eq!(db.read::<String>(&key), Some(value_b.clone()));
+    assert_eq!(db.delete::<String>(&key).ok(), Some(value_b));
+    assert_eq!(db.read::<String>(&key), None);
 }
